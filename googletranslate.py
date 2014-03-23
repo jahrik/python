a186216d01cfc10202a9efe35d8730ddf63733c6
jahrik <jahrik@gmail.com>
2014-03-23 00:19:21 -0600

    working now, will add more language examples later.

--- googletranslate.py
+++ googletranslate.py
@@ -6,14 +6,20 @@ import urllib
 import mechanize
 
 
-get_url = "http://translate.google.com/translate_a/t?client=t&sl=en&tl=es&hl=en&sc=2&ie=UTF-8&oe=UTF-8&prev=btn&ssel=5&tsel=5&q=flower"
+def translate(home_language,target_language,text):
+    text = text.replace(" ","%20")
+    get_url = "http://translate.google.com/translate_a/t?client=t&sl="+home_language+"&tl="+target_language+"&hl="+home_language+"&sc=2&ie=UTF-8&oe=UTF-8&prev=btn&ssel=5&tsel=5&q="+text
+    browser = mechanize.Browser()
+    browser.set_handle_robots(False)
+    browser.addheaders = [('User-agent','Chrome')]
+    translate_text = browser.open(get_url).read().decode('UTF-8')
+    translate_text = translate_text.split("]]")
+    return translate_text[0].replace("[[[","").replace('"','').split(",")[0]
 
-browser = mechanize.Browser()
-browser.set_handle_robots(False)
-browser.addheaders = [('User-agent','Chrome')]
+print "Languages:"
+print "en = English, es = Spanish"
+home_language = raw_input("Home language: ")
+target_language = raw_input("Target language: ")
+text = raw_input("Translate: ")
 
-
-translate_text = browser.open(get_url).read().decode('UTF-8')
-translate_text = translate_text.split("]]")
-
-print translate_text[0].replace("[[[","").replace('"','').split(",")[0]
+print translate(home_language,target_language,text)
