1bbcc4b7c95c69f87e9e095b4c87bd6d1a3e27f0
Dan Daggett <ddaggett@kickbackpoints.com>
2014-03-24 22:06:52 -0600

    Made google libraries into a Python package/modules.

---
+++ web.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+
+from google import search, translate
+
+if __name__ == "__main__":
+#    link = raw_input("Search Google for: ")
+#    print search.getGoogleLinks(link)
+
+    print "Languages"
+    print "en = English, es = Spanish, ja = Japanese"
+#    print getGoogleLanguages()
+    home_language = raw_input("Home language: ")
+    target_language = raw_input("Target language: ")
+    text = raw_input("Translate: ")
+    print translate.translate(home_language,target_language,text)
+
